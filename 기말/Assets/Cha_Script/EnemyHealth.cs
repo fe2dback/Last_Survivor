b0d0e7132486f921f908c2310c41c63090cc070c
Ms <82143640+minsu616@users.noreply.github.com>
2025-11-25 14:33:14 +0900

    Enemy(AI, Controller)스크립트 수정
    적이 플레이어를 쫒아오다 총알에
    맞을시 정지&색깔(red)로 변경,
    죽을시 색깔(gray)변경

--- 기말/Assets/Cha_Script/EnemyHealth.cs
+++ 기말/Assets/Cha_Script/EnemyHealth.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
     Renderer enemyColor;
-
+    
     private EnemyStats enemyStats;
-    private int currentHealth; //현재 체력
+    private float currentHealth; //현재 체력
+    private MelleEnemyAI melleEnemyAI;
 
     void Awake()
     {
+
+        enemyColor = GetComponent<Renderer>();
         enemyStats = GetComponent<EnemyStats>();
-        if(enemyStats != null )
+        melleEnemyAI = GetComponent<MelleEnemyAI>();
+
+        if (melleEnemyAI == null)
+        {
+            Debug.LogError("EnemyHealth: MelleEnemyAI 컴포넌트를 찾을 수 없습니다! 같은 오브젝트에 붙어있나요?", this);
+        }
+
+        //originalEnemyColor = enemyColor.material.color;
+        if (enemyStats != null )
         {
             //EnemyStats 16line에서 받아온 값
             currentHealth = enemyStats.MaxHealth; //EnemyStats에서 계산된 MaxHealth로 초기화
@@ -26,13 +37,18 @@ public class EnemyHealth : MonoBehaviour
 
     private void Start()
     {
-        enemyColor = GetComponent<Renderer>();
+        
     }
 
-    public void TakeDamate(int damage)
+    public void TakeDamage(float damage)
     {
         currentHealth-= damage;
         Debug.Log("적 현재 체력: " + currentHealth);
+
+        if (melleEnemyAI != null)
+        {
+            melleEnemyAI.ApplyHitEffect(0.2f);
+        }
 
         if (currentHealth <= 0)
         {
@@ -46,7 +62,7 @@ public class EnemyHealth : MonoBehaviour
         if (bullet != null)
         {
             // 총알의 데미지 만큼 체력을 깎음
-            //TakeDamate(bullet.damage);
+            TakeDamage(bullet.bulletDamage);
 
             // 총알은 충돌 후 제거
             Destroy(other.gameObject);
@@ -56,8 +72,9 @@ public class EnemyHealth : MonoBehaviour
     private void Die()
     {
         Debug.Log("적이 죽었습니다");
-        enemyColor.material.color = Color.red;
-        Destroy(gameObject, 2f);
+        enemyColor.material.color = Color.gray;
+        Destroy(gameObject, 0.5f);
 
     }
+
 }
